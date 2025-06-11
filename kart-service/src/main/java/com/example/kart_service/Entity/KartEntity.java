a386c7eac15ce04542547652f0fcad8d3c17c489
belen-aedo <belen.aedo@usach.cl>
2025-06-11 15:17:21 -0400

    -

--- kart-service/src/main/java/com/example/kart_service/Entity/KartEntity.java
+++ kart-service/src/main/java/com/example/kart_service/Entity/KartEntity.java
@@ -1,9 +1,11 @@
 package com.example.kart_service.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
 
 @Entity
 public class KartEntity {
